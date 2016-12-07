6fc3b68320c86d7b1ded9f4145d379390fca406f
Mark Fox <Mark Fox@DESKTOP-MRLVIUI>
2016-12-07 14:50:05 -0600

    Added comments and cleaned out unused code

--- Project 12-1 Basic Calculator/Calculator.cs
+++ Project 12-1 Basic Calculator/Calculator.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿// This class provides calculator functionality.
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -7,63 +8,50 @@ using System.Threading.Tasks;
 namespace Project_14_1_Basic_Calculator
 {
     class Calculator
-    { 
-        private decimal input;
-        private List<decimal> inputs = new List<decimal>();
-        private byte oper;      // 1 = ADD; 2 = SUBTRACT; 3 = MULTIPLY; 4 = DIVIDE
+    {
+        // Declares variables
+        private byte oper;
         private decimal total;
         private decimal[] values;
         public bool newInputs;
-        public bool newCalculation;
-        private decimal currentValue = 0;
-        private bool firstInput;
 
         public Calculator()
         {
+            // Creates array to hold inputs
             values = new decimal[2];
         }
 
-        public void EnterValue(string value)
-        {
-            if (isNumeric(value))
-            {
-                input = Convert.ToDecimal(value);
-                inputs.Add(input);
-                if (firstInput == false)
-                {
-                    firstInput = true;
-                }
-                else
-                {
-
-                }
-            }
-        }
         public void Add(decimal value)
         {
+            // Saves passed value as first input and sets operator variable
             values[0] = value;
             oper = 1;
         }
         public void Subtract(decimal value)
         {
+            // Saves passed value as first input and sets operator variable
             values[0] = value;
             oper = 2;
         }
         public void Multiply(decimal value)
         {
+            // Saves passed value as first input and sets operator variable
             values[0] = value;
             oper = 3;
         }
         public void Divide(decimal value)
         {
+            // Saves passed value as first input and sets operator variable
             values[0] = value;
             oper = 4;
         }
         public decimal Equals(decimal value)
         {
+            // Saves passed value as second input and determines what action to take
             values[1] = value;
             switch (oper)
             {
+                // 1 = ADD; 2 = SUBTRACT; 3 = MULTIPLY; 4 = DIVIDE
                 case 1:
                     total = values[0] + values[1];
                     break;
@@ -83,24 +71,27 @@ namespace Project_14_1_Basic_Calculator
         }
         public decimal Reciprocal(decimal value)
         {
+            // Returns reciprocal calculation
             return 1 / value;
         }
         public decimal SquareRoot(decimal value)
         {
+            // Returns square root calculation
             return (decimal)Math.Sqrt((double)value);
         }
         public void Clear()
         {
+            // Clears stored array
             Array.Clear(values, 0, values.Length);
         }
         public bool isNumeric(string input)
         {
+            // Validates inputs
             decimal number = 0m;
             if (Decimal.TryParse(input, out number))
                 return true;
             else
                 return false;
         }
-        
     }
 }
